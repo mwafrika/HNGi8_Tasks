9ce3de2562485c5d82d5fe29c0381ed998c45b3d
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-08-20 14:54:09 +0300

    Delete

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import nodemailer from "nodemailer";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 dotenv.config();
