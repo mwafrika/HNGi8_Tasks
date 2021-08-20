bfca5a814d4155c9c69603b1901e3727258f26b2
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-08-20 17:35:25 +0300

    Restructure the project folder

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/", express.static(path.join(__dirname, "../frontend/build")));
-app.use("/send", route);
+app.use("/", route);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
--- backend/route.js
+++ backend/route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import sendMessage from "./controller";
 
-const router = Router().post("/", sendMessage);
+const router = Router().post("/send", sendMessage);
 
 export default router;
