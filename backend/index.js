b5aee1557e95a043be5f2864ed950c2d5b4ccb14
mwafrika <25848053+mwafrika@users.noreply.github.com>
2021-08-20 17:44:17 +0300

    Restructure the project folder

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import nodemailer from "nodemailer";
 import path from "path";
-import route from "./route";
 import { fileURLToPath } from "url";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 dotenv.config();
 
@@ -17,12 +15,52 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/", express.static(path.join(__dirname, "../frontend/build")));
-app.use("/", route);
 
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+});
+
+transporter.verify((err, success) => {
+  err
+    ? console.log(err)
+    : console.log(`=== Server is ready to take messages: ${success} ===`);
+});
+
+app.post("/send", (req, res) => {
+  const { name, email, message, subject } = req.body;
+  let mailOptions = {
+    from: email,
+    to: process.env.EMAIL,
+    subject: `Message from: ${email}`,
+    text: `${message}`,
+    name: `${name}`,
+    objet: `${subject}`,
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
+    } else {
+      res.json({
+        status: "success",
+      });
+      console.log("Email sent successfully");
+    }
+  });
+});
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- backend/route.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import sendMessage from "./controller";
-
-const router = Router().post("/send", sendMessage);
-
-export default router;
